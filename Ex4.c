3b4087320a99c3916d3f9cedd73bbf244552e06c
PhamVietAn <anpv.b24dtcn296@stu.ptit.edu.vn>
2024-12-19 21:19:02 +0700

    Add files via upload

--- Ex4.c
+++ Ex4.c
@@ -2,13 +2,15 @@
 #include <string.h>
 
 struct sinhVien{
+    int id;
     char name[50];
     int age;
     char phoneNumber[15];
 };
 typedef struct sinhVien sinhVien;
 
-void nhapThongTin(sinhVien *sv){
+void nhapThongTin(sinhVien *sv, int id){
+    sv->id = id;
     printf("Nhap ten: ");
     fgets(sv->name, sizeof(sv->name), stdin);
 
@@ -21,23 +23,24 @@ void nhapThongTin(sinhVien *sv){
 }
 
 void inThongTin(sinhVien sv){
-    printf("Thong tin sinh vien:\n");
+    printf("ID: %d\n", sv.id);
     printf("Name: %s", sv.name);
     printf("Age: %d\n", sv.age);
-    printf("Phone Number: %s", sv.phoneNumber);
+    printf("Phone Number: %s\n", sv.phoneNumber);
 }
 
 int main(){
-    sinhVien danhSach[5];
+    sinhVien danhSach[50];
+    int soLuongSinhVien = 5;
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < soLuongSinhVien; i++){
         printf("Nhap thong tin sinh vien thu %d:\n", i + 1);
-        nhapThongTin(&danhSach[i]);
+        nhapThongTin(&danhSach[i], i + 1);
         printf("\n");
     }
 
     printf("\nThong tin cac sinh vien:\n");
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < soLuongSinhVien; i++){
         printf("\nSinh vien thu %d:\n", i + 1);
         inThongTin(danhSach[i]);
     }
